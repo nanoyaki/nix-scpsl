ce66b26e771277afb288d8c46bd2cae67b25a3aa
nanoyaki <hanakretzer@nanoyaki.space>
2025-12-28 19:46:15 +0100

    fix(auto-update): use steamcmd api for the manifest id update

--- pkgs/scpsl-server/steam-update.sh
+++ pkgs/scpsl-server/steam-update.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash --expr 'with import <nixpkgs> { config.allowUnfree = true; }; runCommand "dummy" { buildInputs = [ steamcmd coreutils gnugrep gnused ]; } ""'
+#!nix-shell -i bash -p depotdownloader coreutils gnugrep gnused
 
 APP_ID="$1"
 DEPOT_ID="$2"
 PACKAGE_PATH="$3"
 
 MANIFEST_ID="$(
-    steamcmd +login anonymous \
-    +app_info_print "$APP_ID" \
-    +quit \
-    | grep -A 20 "\"$DEPOT_ID\"" \
-    | grep '"gid"' | head -n 1 | sed 's/[^0-9]//g'
+    curl -s "https://api.steamcmd.net/v1/info/$APP_ID" \
+    | jq -r \
+    --arg appId "$APP_ID" \
+    --arg depotId "$DEPOT_ID" \
+    '.data[$appId].depots[$depotId].manifests.public.gid'
 )"
 
 sed -i 's/manifestId = "[0-9]*"/manifestId = "'"$MANIFEST_ID"'"/' "$PACKAGE_PATH"
